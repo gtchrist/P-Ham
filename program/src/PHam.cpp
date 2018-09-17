4964b8322d31feca7b655404abb7f3ed8d9d3c55
Wyatt <wyatt@digitalcave.ca>
2018-09-16 18:46:14 -0600

    Output ADC value to serial port for debugging

--- program/src/PHam.cpp
+++ program/src/PHam.cpp
@@ -1,49 +1,49 @@
 #include "PHam.h"
 
+#include <stdio.h>
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
 
-#include <SerialAVR.h>
+#include <SerialUSB.h>
 
 #include "lib/analog/analog.h"
 
 using namespace digitalcave;
 
-//Reset WDT after system reset
-void get_mcusr(void) __attribute__((naked))  __attribute__((used))  __attribute__((section(".init3")));
-void get_mcusr(void) {
-	MCUSR = 0;
-	wdt_disable();
-}
-
 int main() __attribute__ ((noreturn));
 int main(){
-	// DISABLE JTAG - take control of F port
+	// Disable JTAG so we can use PORTF
 	MCUCR = _BV(JTD);
 	MCUCR = _BV(JTD);
-	
-	//Init the serial port for debugging
-	SerialAVR serial(38400, 8, 0, 1, 1, 128);
 	
 	//Turn on PORT B4 and B5 for output
 	DDRB |= _BV(4) | _BV(5);
+	PORTB |= _BV(4);
+	
+	//Init the USB serial port for debugging.
+	SerialUSB serialUSB;
 	
 	//Init ADC pins with reference of AVCC (with external cap at AREF).
 	uint8_t analog_pins[1];
 	analog_pins[0] = 0;		//F0
 	analog_init(analog_pins, 1, ANALOG_AVCC);
 	
-	PORTB |= _BV(5);
-	_delay_ms(100);
 	PORTB &= ~_BV(5);
+	_delay_ms(1000);
+	PORTB |= _BV(5);
 	
 	//Enable watchdog timer
 	wdt_enable(WDTO_500MS);
 
 	while(1){
 		wdt_reset();
-//		serial.write("Foo\n");
+		if (serialUSB.isConnected()){
+			char temp[32];
+			snprintf(temp, sizeof(temp), "ADC: %d\n", analog_read_p(0));
+			serialUSB.write(temp);
+		}
+
 		PORTB ^= _BV(4);
 		_delay_ms(100);
 	}
