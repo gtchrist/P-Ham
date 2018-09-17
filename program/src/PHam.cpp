ef61a2a7140605d70303cda20c487caaf4f1f3f5
Wyatt <wyatt@digitalcave.ca>
2018-09-17 15:48:59 -0600

    Added in servo control value which maps to potentiometer value.  Untested but compiles.

--- program/src/PHam.cpp
+++ program/src/PHam.cpp
@@ -8,6 +8,7 @@
 #include <SerialUSB.h>
 
 #include "lib/analog/analog.h"
+#include "lib/pwm/pwm.h"
 
 using namespace digitalcave;
 
@@ -26,9 +27,17 @@ int main(){
 	
 	//Init ADC pins with reference of AVCC (with external cap at AREF).
 	uint8_t analog_pins[1];
-	analog_pins[0] = 0;		//F0
+	analog_pins[0] = ANALOG_PIN;
 	analog_init(analog_pins, 1, ANALOG_AVCC);
 	
+	//Init the PWM timer
+	volatile uint8_t *ports[1];
+	uint8_t pins[1];
+	ports[0] = &SERVO_PORT;
+	pins[0] = SERVO_PIN;
+	pwm_init(ports, pins, 1, 20000);
+	
+	//Flash a blue light for one second at startup - this shows us clearly if there has been a reset due to WDT or something.
 	PORTB &= ~_BV(5);
 	_delay_ms(1000);
 	PORTB |= _BV(5);
@@ -38,13 +47,19 @@ int main(){
 
 	while(1){
 		wdt_reset();
+		uint16_t value = analog_read_p(0);
 		if (serialUSB.isConnected()){
 			char temp[32];
-			snprintf(temp, sizeof(temp), "ADC: %d\n", analog_read_p(0));
+			snprintf(temp, sizeof(temp), "ADC: %d\n", value);
 			serialUSB.write(temp);
 		}
+		
+		//The ADC value is a 10 bit value; a servo PWM signal has a phase from 1000us to 2000us (with a period of 20000us).
+		// A 10 bit value is slightly over spec, but a) most servos accept overdriven values anyway, and b) this is just a test.
+		pwm_set_phase(0, 1000 + value);
 
+		//Flash a heartbeat every 200ms
 		PORTB ^= _BV(4);
-		_delay_ms(100);
+		_delay_ms(200);
 	}
 }
--- program/src/PHam.h
+++ program/src/PHam.h
@@ -6,4 +6,11 @@
 
 #define DEBUG
 
+//The port / pin to be used for the servo
+#define SERVO_PORT			PORTC
+#define SERVO_PIN			6
+
+//The ADC pin to be used for the pedal.  ADC 0 is pin F0
+#define ANALOG_PIN			0
+
 #endif
