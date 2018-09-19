178de108ae1fc1f9807023b575996eba551b2542
Tyler Christensen <tylerchristensen@Tylers-MBP.digitalcave.local>
2018-09-18 22:22:56 -0600

    removed gitter from servo and added documentation

--- program/src/PHam.cpp
+++ program/src/PHam.cpp
@@ -17,49 +17,50 @@ int main(){
 	// Disable JTAG so we can use PORTF
 	MCUCR = _BV(JTD);
 	MCUCR = _BV(JTD);
-	
+
 	//Turn on PORT B4 and B5 for output
 	DDRB |= _BV(4) | _BV(5);
 	PORTB |= _BV(4);
-	
+
 	//Init the USB serial port for debugging.
 	SerialUSB serialUSB;
-	
+
 	//Init ADC pins with reference of AVCC (with external cap at AREF).
 	uint8_t analog_pins[1];
 	analog_pins[0] = ANALOG_PIN;
 	analog_init(analog_pins, 1, ANALOG_AVCC);
-	
+
 	//Init the PWM timer
 	volatile uint8_t *ports[1];
 	uint8_t pins[1];
 	ports[0] = &SERVO_PORT;
 	pins[0] = SERVO_PIN;
-	pwm_init(ports, pins, 1, 20000);
-	
+	pwm_init(ports, pins, 1, 10000);
+
 	//Flash a blue light for one second at startup - this shows us clearly if there has been a reset due to WDT or something.
 	PORTB &= ~_BV(5);
 	_delay_ms(1000);
 	PORTB |= _BV(5);
-	
+
 	//Enable watchdog timer
 	wdt_enable(WDTO_500MS);
+	uint16_t value = analog_read_p(0);
 
 	while(1){
 		wdt_reset();
-		uint16_t value = analog_read_p(0);
+		value = analog_read_p(0)  & 0X3FC;
 		if (serialUSB.isConnected()){
 			char temp[32];
 			snprintf(temp, sizeof(temp), "ADC: %d\n", value);
 			serialUSB.write(temp);
 		}
-		
+
 		//The ADC value is a 10 bit value; a servo PWM signal has a phase from 1000us to 2000us (with a period of 20000us).
 		// A 10 bit value is slightly over spec, but a) most servos accept overdriven values anyway, and b) this is just a test.
 		pwm_set_phase(0, 1000 + value);
 
 		//Flash a heartbeat every 200ms
 		PORTB ^= _BV(4);
-		_delay_ms(20);
+		_delay_ms(100);
 	}
 }
--- program/src/lib/analog/analog_synchronous.c
+++ program/src/lib/analog/analog_synchronous.c
@@ -2,7 +2,7 @@
 
 #include <avr/interrupt.h>
 
-//This is a 3 bit value; refer to the data sheet for what each mask means.  A 
+//This is a 3 bit value; refer to the data sheet for what each mask means.  A
 // reasonable default seems to be 0x4 (/16) or 0x5 (/32).  Higher prescalers
 // will give more accurate results, at the cost of longer conversion times.
 //Note that a beneficial side-effect of larger ADC values is that the ISR will
@@ -18,7 +18,7 @@ static uint8_t pin_count;
 //Buffer for result values
 static volatile uint16_t results[8];
 
-//Counter for the ISR to know which ADC it is talking to at a given time.  Used to 
+//Counter for the ISR to know which ADC it is talking to at a given time.  Used to
 // configure the ADC mux channel section.  This value refers to the index of
 // the pins variable.
 static uint8_t adc_mux = 0;
@@ -46,10 +46,10 @@ void analog_init(uint8_t analog_pins[], uint8_t count, uint8_t aref){
 
 	//Free running mode (already default, but in case it was set previously)
 	ADCSRB = 0x00;
-	
+
 	//Start ADC conversions
-	ADCSRA |= _BV(ADSC);	
-	
+	ADCSRA |= _BV(ADSC);
+
 	//Enable interrupts if the NO_INTERRUPT_ENABLE define is not set.  If it is, you need to call sei() elsewhere.
 #ifndef NO_INTERRUPT_ENABLE
 	sei();
@@ -68,6 +68,7 @@ uint16_t analog_read_p(uint8_t index){
 }
 
 ISR(ADC_vect){
+	sei();
 	//Read last ADC value assuming calibration is finished
 	results[adc_mux] = ADC;
 
